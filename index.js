13bd293094031533ca60cd473ca619e7248a6d1d
gat786 <ganesht049@gmail.com>
2020-02-10 09:21:42 +0530

    changed theme

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const path = require("path")
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
+
+app.locals.basedir = __dirname
+
 app.set('view engine','pug')
 app.get('/',function (req, res) {
     res.render('hello', { title: 'Hello Vartak College', message: 'Hey There!' })
@@ -12,5 +15,7 @@ app.get('/',function (req, res) {
 )
 
 const server = http.createServer(app)
+
+const port = 3000
 
 server.listen(8080)
